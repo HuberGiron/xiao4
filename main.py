ed9e284f17d6124bbf60796023c48fe5e02781e7
Huber Giron <125147932+HuberGiron@users.noreply.github.com>
2025-06-27 16:56:23 +0000

    3 camaras

--- main.py
+++ main.py
@@ -19,17 +19,40 @@ app.add_middleware(
 )
 
 # ✅ Endpoint optimizado para subir imagen JPEG directa (sin multipart)
-@app.post("/upload")
-async def upload_image(request: Request):
+@app.post("/upload1")
+async def upload_image1(request: Request):
     image_data = await request.body()
-    with open("static/latest.jpg", "wb") as buffer:
+    with open("static/latest1.jpg", "wb") as buffer:
         buffer.write(image_data)
     return {"status": "ok"}
 
+@app.post("/upload2")
+async def upload_image2(request: Request):
+    image_data = await request.body()
+    with open("static/latest2.jpg", "wb") as buffer:
+        buffer.write(image_data)
+    return {"status": "ok"}
+
+@app.post("/upload3")
+async def upload_image3(request: Request):
+    image_data = await request.body()
+    with open("static/latest3.jpg", "wb") as buffer:
+        buffer.write(image_data)
+    return {"status": "ok"}
+
+
 # ✅ Servir la última imagen
-@app.get("/latest.jpg")
-def get_image():
-    return FileResponse("static/latest.jpg", headers={"Cache-Control": "no-store"})
+@app.get("/latest1.jpg")
+def get_image1():
+    return FileResponse("static/latest1.jpg", headers={"Cache-Control": "no-store"})
+
+@app.get("/latest2.jpg")
+def get_image2():
+    return FileResponse("static/latest2.jpg", headers={"Cache-Control": "no-store"})
+
+@app.get("/latest3.jpg")
+def get_image3():
+    return FileResponse("static/latest3.jpg", headers={"Cache-Control": "no-store"})
 
 # ✅ WebSocket opcional
 @app.websocket("/ws")
